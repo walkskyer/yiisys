05a2fe19becb4de33d3a1be5d7f473979fc73900
walkskyer <walkskyer@qq.com>
2013-04-17 19:13:10 +0800

    后台添加分类增删改操作。

---
+++ public/protected/modules/admin/views/category/update.php
@@ -0,0 +1,18 @@
+<?php
+$this->breadcrumbs=array(
+	'Categories'=>array('index'),
+	$model->catid=>array('view','id'=>$model->catid),
+	'Update',
+);
+
+$this->menu=array(
+	array('label'=>'List Category', 'url'=>array('index')),
+	array('label'=>'Create Category', 'url'=>array('create')),
+	array('label'=>'View Category', 'url'=>array('view', 'id'=>$model->catid)),
+	array('label'=>'Manage Category', 'url'=>array('admin')),
+);
+?>
+
+<h1>Update Category <?php echo $model->catid; ?></h1>
+
+<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
