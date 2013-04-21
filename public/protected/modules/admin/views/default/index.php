23adf3fcbe650f798a1ad5fec650f86d211c5c8c
walkskyer <walkskyer@qq.com>
2013-04-21 17:57:21 +0800

    管理后台界面简化并统一语言配置。

--- public/protected/modules/admin/views/default/index.php
+++ public/protected/modules/admin/views/default/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo app()->charset;?>" />
-<title><?php echo app()->name . t('control_center', 'admin');?></title>
+<title><?php echo app()->name . t('control_center', 'sys_admin');?></title>
 <link rel="stylesheet" type="text/css" href="<?php echo sbu('libs/bootstrap/css/bootstrap.min.css');?>" />
 <link rel="stylesheet" type="text/css" href="<?php echo sbu('styles/sys-admin.css');?>" />
 <script type="text/javascript" src="<?php echo sbu('libs/jquery.min.js');?>"></script>
@@ -11,127 +11,84 @@
 <div class="navbar navbar-fixed-top">
     <div class="navbar-inner">
         <div class="container admin-nav-container">
-            <a class="brand" href="<?php echo url('admin/default/welcome');?>" target="main"><?php echo t('control_center', 'admin');?></a>
+            <a class="brand" href="<?php echo url('admin/default/welcome');?>" target="main"><?php echo t('control_center', 'sys_admin');?></a>
             <ul class="nav">
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo t('post_manage', 'admin');?><b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo t('content_manage', 'sys_admin');?><b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li><?php echo l(t('create_posts', 'admin'), url('admin/post/create'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('verify_posts', 'admin'), url('admin/post/verify'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('search_posts', 'admin'), url('admin/post/search'), array('target'=>'main'));?></li>
+                        <li><?php echo l(t('content_create', 'sys_admin'), url('admin/content/create'), array('target'=>'main'));?></li>
+                        <li><?php echo l(t('content_manage', 'sys_admin'), url('admin/content/index'), array('target'=>'main'));?></li>
                         <li class="divider"></li>
-                        <li><?php echo l(t('latest_posts', 'admin'), url('admin/post/latest'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('hottest_posts', 'admin'), url('admin/post/hottest'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('editor_recommend_posts', 'admin'), url('admin/post/recommend'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('home_show_posts', 'admin'), url('admin/post/homeshow'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('istop_posts', 'admin'), url('admin/post/istop'), array('target'=>'main'));?></li>
+                        <li><?php echo l(t('latest_posts', 'sys_admin'), url('admin/post/latest'), array('target'=>'main'));?></li>
+                        <li><?php echo l(t('hottest_posts', 'sys_admin'), url('admin/post/hottest'), array('target'=>'main'));?></li>
+                        <li><?php echo l(t('editor_recommend_posts', 'sys_admin'), url('admin/post/recommend'), array('target'=>'main'));?></li>
+                        <li><?php echo l(t('home_show_posts', 'sys_admin'), url('admin/post/homeshow'), array('target'=>'main'));?></li>
+                        <li><?php echo l(t('istop_posts', 'sys_admin'), url('admin/post/istop'), array('target'=>'main'));?></li>
                         <li class="divider"></li>
                         <li class="nav-header">附件</li>
-                        <li><?php echo l(t('upload_file_search', 'admin'), url('admin/upload/search'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('upload_file_list', 'admin'), url('admin/upload/list'), array('target'=>'main'));?></li>
+                        <li><?php echo l(t('upload_file_search', 'sys_admin'), url('admin/upload/search'), array('target'=>'main'));?></li>
+                        <li><?php echo l(t('upload_file_list', 'sys_admin'), url('admin/upload/list'), array('target'=>'main'));?></li>
                         <li class="divider"></li>
-                        <li><?php echo l(t('trash', 'admin'), url('admin/post/trash'), array('target'=>'main'));?></li>
+                        <li><?php echo l(t('trash', 'sys_admin'), url('admin/post/trash'), array('target'=>'main'));?></li>
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo t('topic_category', 'admin');?><b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo t('category_manage', 'sys_admin');?><b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li class="nav-header"><?php echo t('post_topic', 'admin');?></li>
-                        <li><?php echo l(t('create_topic', 'admin'), url('admin/topic/create'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('topic_list_table', 'admin'), url('admin/topic/list'), array('target'=>'main'));?></li>
-                        <!-- <li><?php echo l(t('topic_statistics', 'admin'), url('admin/topic/statistics'), array('target'=>'main'));?></li> -->
-                        <li class="divider"></li>
-                        <li class="nav-header"><?php echo t('post_special', 'admin');?></li>
-                        <li><?php echo l(t('create_special', 'admin'), url('admin/special/create'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('special_list_table', 'admin'), url('admin/special/list'), array('target'=>'main'));?></li>
-                        <li class="divider"></li>
-                        <li class="nav-header"><?php echo t('post_category', 'admin');?></li>
-                        <li><?php echo l(t('create_category', 'admin'), url('admin/category/create'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('category_list_table', 'admin'), url('admin/category/index'), array('target'=>'main'));?></li>
-                        <!-- <li><?php echo l(t('category_statistics', 'admin'), url('admin/category/statistics'), array('target'=>'main'));?></li> -->
+                        <li class="nav-header"><?php echo t('category_name', 'sys_admin');?></li>
+                        <li><?php echo l(t('category_create', 'sys_admin'), url('admin/category/create'), array('target'=>'main'));?></li>
+                        <li><?php echo l(t('category_manage', 'sys_admin'), url('admin/category/index'), array('target'=>'main'));?></li>
+                        <!-- <li><?php echo l(t('category_statistics', 'sys_admin'), url('admin/category/statistics'), array('target'=>'main'));?></li> -->
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo t('comment_manage', 'admin');?><b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo t('comment_manage', 'sys_admin');?><b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li class="nav-header"><?php echo t('comment_manage', 'admin');?></li>
-                        <li><?php echo l(t('latest_comment', 'admin'), url('admin/comment/latest'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('verify_comment', 'admin'), url('admin/comment/verify'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('recommend_comment', 'admin'), url('admin/comment/recommend'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('search_comment', 'admin'), url('admin/comment/search'), array('target'=>'main'));?></li>
-                        <li class="divider"></li>
-                        <li class="nav-header"><?php echo t('post_tag', 'admin');?></li>
-                        <li><?php echo l(t('hottest_tags', 'admin'), url('admin/tag/hottest'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('tag_search', 'admin'), url('admin/tag/search'), array('target'=>'main'));?></li>
+                        <li class="nav-header"><?php echo t('post_tag', 'sys_admin');?></li>
+                        <li><?php echo l(t('hottest_tags', 'sys_admin'), url('admin/tag/hottest'), array('target'=>'main'));?></li>
+                        <li><?php echo l(t('tag_search', 'sys_admin'), url('admin/tag/search'), array('target'=>'main'));?></li>
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo t('user_manage', 'admin');?><b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo t('user_manage', 'sys_admin');?><b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li><?php echo l(t('create_user', 'admin'), url('admin/user/create'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('verify_user', 'admin'), url('admin/user/verify'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('search_user', 'admin'), url('admin/user/search'), array('target'=>'main'));?></li>
-                        <li class="divider"></li>
-                        <li class="nav-header"><?php echo t('statistics', 'admin');?></li>
-                        <li><?php echo l(t('today_signup_user', 'admin'), url('admin/user/today'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('user_account_list', 'admin'), url('admin/user/list'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('forbidden_user', 'admin'), url('admin/user/forbidden'), array('target'=>'main'));?></li>
+                        <li><?php echo l(t('user_create', 'sys_admin'), url('admin/user/create'), array('target'=>'main'));?></li>
+                        <li><?php echo l(t('user_manage', 'sys_admin'), url('admin/user/search'), array('target'=>'main'));?></li>
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo t('system_tool', 'admin');?><b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo t('system_tool', 'sys_admin');?><b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <!--
-                        <li class="nav-header">广告</li>
-                        <li><a href="#">广告单元</a></li>
-                        <li><a href="#">自建广告</a></li>
-                        <li class="divider"></li>
-                        <li class="nav-header">数据</li>
-                        <li><a href="#">备份</a></li>
-                        <li><a href="#">恢复</a></li>
-                        <li class="divider"></li>
-                        -->
-                        <li><?php echo l(t('friend_link', 'admin'), url('admin/link/list'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('advert_managent', 'admin'), url('admin/advert/list'), array('target'=>'main'));?></li>
+                        <li><?php echo l(t('friend_link', 'sys_admin'), url('admin/link/list'), array('target'=>'main'));?></li>
+                        <li><?php echo l(t('advert_managent', 'sys_admin'), url('admin/advert/list'), array('target'=>'main'));?></li>
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo t('system_setting', 'admin');?><b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo t('system_setting', 'sys_admin');?><b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li class="nav-header"><?php echo t('filter_keyword_manage', 'admin');?></li>
-                        <li><?php echo l(t('multi_create_filter_keyword', 'admin'), url('admin/keyword/create'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('filter_keyword_list', 'admin'), url('admin/keyword/list'), array('target'=>'main'));?></li>
-                        <li class="divider"></li>
-                        <li class="nav-header"><?php echo t('custom_config_params', 'admin');?></li>
-                        <li><?php echo l(t('create_custom_param', 'admin'), url('admin/config/create'), array('target'=>'main'));?></li>
-                        <li class="divider"></li>
-                        <li class="nav-header"><?php echo t('navigation_name', 'admin');?></li>
-                        <li><?php echo l(t('navigation_create', 'admin'), url('admin/navigation/create'), array('target'=>'main'));?></li>
-                        <li><?php echo l(t('navigation_manage', 'admin'), url('admin/navigation/index'), array('target'=>'main'));?></li>
+                        <li class="nav-header"><?php echo t('navigation_name', 'sys_admin');?></li>
+                        <li><?php echo l(t('navigation_create', 'sys_admin'), url('admin/navigation/create'), array('target'=>'main'));?></li>
+                        <li><?php echo l(t('navigation_manage', 'sys_admin'), url('admin/navigation/index'), array('target'=>'main'));?></li>
                     </ul>
                 </li>
             </ul>
             <ul class="nav pull-right">
                 <li><?php echo l(user()->name, url('admin/user/current'), array('target'=>'main'));?></li>
-                <li><?php echo l(t('logout_control_center', 'admin'), url('site/logout'));?></li>
-                <li><?php echo l(t('site_home', 'admin'), url('site/index'), array('target'=>'_blank'));?></li>
+                <li><?php echo l(t('logout_control_center', 'sys_admin'), url('site/logout'));?></li>
+                <li><?php echo l(t('site_home', 'sys_admin'), url('site/index'), array('target'=>'_blank'));?></li>
             </ul>
         </div>
     </div>
 </div>
 <div class="well admin-sidebar">
     <ul class="nav nav-list">
-        <li class="nav-header"><?php echo t('article', 'admin');?></li>
-        <li><?php echo l(t('create_posts', 'admin'), url('admin/content/create'), array('target'=>'main'));?></li>
-        <li><?php echo l(t('verify_posts', 'admin'), url('admin/post/verify'), array('target'=>'main'));?></li>
-        <li><?php echo l(t('latest_posts', 'admin'), url('admin/post/latest'), array('target'=>'main'));?></li>
-        <li><?php echo l(t('manage_posts', 'admin'), url('admin/content/index'), array('target'=>'main'));?></li>
-        <li class="nav-header"><?php echo t('post_comment', 'admin');?></li>
-        <li><?php echo l(t('verify_comment', 'admin'), url('admin/comment/verify'), array('target'=>'main'));?></li>
-        <li><?php echo l(t('latest_comment', 'admin'), url('admin/comment/latest'), array('target'=>'main'));?></li>
-        <li class="nav-header"><?php echo t('user_manage', 'admin');?></li>
-        <li><?php echo l(t('verify_user', 'admin'), url('admin/user/verify'), array('target'=>'main'));?></li>
-        <li><?php echo l(t('today_signup_user', 'admin'), url('admin/user/today'), array('target'=>'main'));?></li>
+        <li class="nav-header"><?php echo t('content_name', 'sys_admin');?></li>
+        <li><?php echo l(t('content_create', 'sys_admin'), url('admin/content/create'), array('target'=>'main'));?></li>
+        <li><?php echo l(t('content_manage', 'sys_admin'), url('admin/content/index'), array('target'=>'main'));?></li>
+        <li class="divider"></li>
+        <li class="nav-header"><?php echo t('navigation_name', 'sys_admin');?></li>
+        <li><?php echo l(t('navigation_create', 'sys_admin'), url('admin/navigation/create'), array('target'=>'main'));?></li>
+        <li><?php echo l(t('navigation_manage', 'sys_admin'), url('admin/navigation/index'), array('target'=>'main'));?></li>
     </ul>
 </div>
 <div class="admin-container">
